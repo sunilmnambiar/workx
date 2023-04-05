16abbc9a36db51a8ea86c42bb075447f1a028d9d
Sunil Manheri <smanheri@Sunils-MacBook-Pro.local>
2023-04-05 10:21:44 +0530

    initial commit

---
+++ src/main/java/com/sunil/workx/api/WorkXApiApplication.java
@@ -0,0 +1,13 @@
+package com.sunil.workx.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkXApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkXApiApplication.class, args);
+	}
+
+}
